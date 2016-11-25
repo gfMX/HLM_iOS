c9308a8dd73359e2ed3ced2a9eac5b6e149a46b1
gfMX <grafica.mx@gmail.com>
2016-11-25 15:28:57 -0600

    Showing Thumbnails OK, Missing Sync with FullImage

--- HotLikeMe/SelectProfilePicCollectionViewController.swift
+++ HotLikeMe/SelectProfilePicCollectionViewController.swift
@@ -14,6 +14,7 @@ private let reuseIdentifier = "cellFireView"
 class SelectProfilePicCollectionViewController: UICollectionViewController {
     
     var thumbUrls = [String]()
+    var imageUrls = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +34,7 @@ class SelectProfilePicCollectionViewController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -41,18 +42,17 @@ class SelectProfilePicCollectionViewController: UICollectionViewController {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        
         return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
+    
         return self.thumbUrls.count
     }
 
@@ -60,9 +60,9 @@ class SelectProfilePicCollectionViewController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FireCollectionViewCell
     
         // Configure the cell
-        //Helper.loadImageFromUrl(url: thumbUrls[indexPath.item], view: cell.imageThumb)
+        Helper.loadImageFromUrl(url: thumbUrls[indexPath.item], view: cell.imageThumb)
         cell.imageThumb.contentMode = UIViewContentMode.scaleAspectFill;
-        cell.backgroundColor = UIColor.cyan
+        cell.backgroundColor = UIColor.lightGray
         cell.layer.cornerRadius = 8
     
         return cell
@@ -77,12 +77,17 @@ class SelectProfilePicCollectionViewController: UICollectionViewController {
     }
     */
 
-    /*
+    
     // Uncomment this method to specify if the specified item should be selected
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        print(indexPath.item)
+        print(imageUrls[indexPath.item])
+    
+        showImage(newUrl: imageUrls[indexPath.item])
+        
         return true
     }
-    */
+
 
     /*
     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
@@ -108,10 +113,19 @@ class SelectProfilePicCollectionViewController: UICollectionViewController {
         ref.child("users").child(userID!).child("thumbs").observeSingleEvent(of: .value, with: { (snapshot) in
             // Get user value
             let value = snapshot.value as? NSDictionary
-            let thumbsStorage = value?.allValues as! [String]
+            print (value ?? "No values found")
+            let thumbsStorage = value?.allKeys as! [String]
             //let username = value?["alias"] as! String
-            print("Thumbs found: " + (self.thumbUrls.count.description))
-            print(self.thumbUrls)
+            print("Thumbs found: " + (thumbsStorage.count.description))
+            //print(thumbsStorage)
+            
+            
+            //let nCount = thumbsStorage.count
+            //for _ in 0 ..< nCount {
+                //self.thumbUrls = [String](repeating: " ", count: nCount) //.append("") //
+                //self.imageUrls = [String](repeating: " ", count: nCount)
+            //}
+ 
             
             self.getFirePicsUrls(storage: thumbsStorage)
             //self.collectionView?.reloadData()
@@ -122,23 +136,91 @@ class SelectProfilePicCollectionViewController: UICollectionViewController {
     }
     
     func getFirePicsUrls(storage: Array<Any>){
+        print ("Storage count: " + storage.count.description)
+        
         for i in 0 ..< storage.count{
-        FireConnection.storageReference.child(storage[i] as! String).downloadURL { (URL, error) -> Void in
-            if (error != nil) {
-                // Handle any errors
-            } else {
-                // Get the download URL for 'images/stars.jpg'
+            //print (storage[i] as! String)
+            FireConnection.storageReference.child(FireConnection.fireUser.uid).child("/images/thumbs/thumb_" + (storage[i] as! String) + ".jpg").downloadURL { (URL, error) -> Void in
+                if (error != nil) {
+                    print ("An error ocurred!")
+                } else {
+                    // Get the download URL for 'images/stars.jpg'
+                    self.thumbUrls.append((URL?.absoluteString)!) //.insert((URL?.absoluteString)!, at: i) //
+                    //print (URL ?? "Not a valid URL was found")
+                    
+                }
+                if i == (storage.count - 1) {
+                    print("Updating the view")
+                    self.collectionView?.reloadData()
+                }
+            }
+            
+            FireConnection.storageReference.child(FireConnection.fireUser.uid).child("/images/image_" + (storage[i] as! String) + ".jpg").downloadURL { (URL, error) -> Void in
+                if (error != nil) {
+                    print ("An error ocurred!")
+                } else {
+                    self.imageUrls.append((URL?.absoluteString)!) //.insert((URL?.absoluteString)!, at: i) //
+                    //print (URL ?? "Not a valid URL was found")
+                }
             }
         }
-        }
+    }
+    
+    func showImage(newUrl: String){
+        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissImage(sender:)))
+        
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        blurEffectView.tag = 23
+        
+        let newImageView: UIImageView = UIImageView()
+        newImageView.frame = self.view.frame
+        //newImageView.backgroundColor = .black
+        newImageView.contentMode = .scaleAspectFit
+        newImageView.isUserInteractionEnabled = true
+        
+        Helper.loadImageFromUrl(url: newUrl, view: newImageView)
+        blurEffectView.addSubview(newImageView)
+        
+        let btnOk: UIButton = UIButton(frame: CGRect(x: 10, y: (self.view.frame.height-70), width: 100, height: 50))
+        btnOk.setTitle("OK", for: .normal)
+        btnOk.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        btnOk.tag = 1
+        blurEffectView.addSubview(btnOk)
+        
+        let btnCancel: UIButton = UIButton(frame: CGRect(x: (self.view.frame.width-120), y: (self.view.frame.height-70), width: 100, height: 50))
+        btnCancel.setTitle("Cancel", for: .normal)
+        btnCancel.addTarget(self, action: #selector(buttonActionCancel), for: .touchUpInside)
+        btnCancel.tag = 2
+        blurEffectView.addSubview(btnCancel)
+        
+        self.view.addSubview(blurEffectView)
+        
+        blurEffectView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func dismissImage(sender: UITapGestureRecognizer) {
+        print("Image Dismissed!")
+        sender.view?.removeFromSuperview()
+    }
+    
+    func buttonAction(sender: UIButton){
+         self.view.viewWithTag(23)?.removeFromSuperview()
+    }
+    
+    func buttonActionCancel(sender: UIButton){
+        self.view.viewWithTag(23)?.removeFromSuperview()
     }
 
-    
     @IBAction func goBack(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
+        
     }
     
     @IBAction func deleteImage(_ sender: UIBarButtonItem) {
+        
     }
 
 }
